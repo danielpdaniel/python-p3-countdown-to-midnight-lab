b20604b14f0bf09fab1ef2890b122535dd9243fa
danielpdaniel <artofjquinn@gmail.com>
2023-06-27 20:02:06 -0400

    Update functions to get tests passing

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,18 @@
 # your code goes here!
+import time
+
+def countdown(int):
+    i = int
+    while i > 0:
+        print(f"{i} SECOND(S)!")
+        i -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(int):
+    i = int
+    while i > 0:
+        print(f"{i} SECOND(S)!")
+        i -= 1
+        time.sleep(1)
+    time.sleep(1)
+    print("HAPPY NEW YEAR!")
